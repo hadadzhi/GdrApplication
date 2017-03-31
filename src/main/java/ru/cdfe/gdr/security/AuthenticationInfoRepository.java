c25c4f95dc3b11b2e3aeb555653994726588a9a4
hadadzhi <hadadzhi@gmail.com>
2017-03-31 22:59:10 +0300

    extract interface AuthenticationInfoRepository

--- src/main/java/ru/cdfe/gdr/security/AuthenticationInfoRepository.java
+++ src/main/java/ru/cdfe/gdr/security/AuthenticationInfoRepository.java
@@ -1,41 +1,27 @@
 package ru.cdfe.gdr.security;
 
-import org.springframework.stereotype.Component;
-
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-
 /**
  * Maps {@link String} tokens to {@link AuthenticationInfo} instances
  * representing the user identified by the token.
  */
-@Component
-public class AuthenticationInfoRepository {
-    private final ConcurrentMap<String, AuthenticationInfo> map = new ConcurrentHashMap<>();
-    
+public interface AuthenticationInfoRepository {
     /**
      * Returns the authentication for the specified token or {@code null}
      * if there is no authentication for this token.
      */
-    public AuthenticationInfo get(String token) {
-        return map.get(token);
-    }
+    AuthenticationInfo get(String token);
     
     /**
      * Associates the specified authentication with the specified token.
      * @return the previous authentication associated with the token, or {@code null}
      * if there was no authentication for the specified token.
      */
-    public AuthenticationInfo put(String token, AuthenticationInfo authInfo) {
-        return map.put(token, authInfo);
-    }
+    AuthenticationInfo put(String token, AuthenticationInfo authInfo);
     
     /**
      * Removes the authentication for the specified token.
      * @return the previous authentication associated with the token, or {@code null}
      * if there was no authentication for the specified token.
      */
-    public AuthenticationInfo remove(String token) {
-        return map.remove(token);
-    }
+    AuthenticationInfo remove(String token);
 }
