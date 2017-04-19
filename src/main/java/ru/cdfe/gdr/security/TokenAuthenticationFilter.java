56028f6f533e17885caac258b179d66d34cb894b
hadadzhi <hadadzhi@gmail.com>
2017-04-19 22:37:09 +0300

    formatter

--- src/main/java/ru/cdfe/gdr/security/TokenAuthenticationFilter.java
+++ src/main/java/ru/cdfe/gdr/security/TokenAuthenticationFilter.java
@@ -37,9 +37,7 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     }
     
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         
         if (SecurityContextHolder.getContext().getAuthentication() == null) {
